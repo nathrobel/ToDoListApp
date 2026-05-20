d68ef9e5559178a4c3ee41068f67dce412edca8c
Nathan Robel <nathanrobel6@gmail.com>
2026-05-20 16:42:02 +0100

    HTML page served

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask is Working"
+    #“Go into the templates folder, find index.html, and send it to the browser.
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
