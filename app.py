4bf91e330bc5c60de2c9d1fe68b4f7da91fe2014
Nathan Robel <nathanrobel6@gmail.com>
2026-05-20 17:27:32 +0100

    created form

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 
 app = Flask(__name__)
+tasks = []
 
 @app.route("/")
 def home():
     #“Go into the templates folder, find index.html, and send it to the browser.
-    tasks = ["Buy milk", "Study Flask"]
+    
     return render_template("index.html",tasks= tasks)
+
+@app.route("/add", methods = ["POST"])
+def add():
+    #“Go into the templates folder, find index.html, and send it to the browser.
+     # get data from form
+    task = request.form["task"]
+    tasks.append(task)
+    
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
