aac6f0d94b58c6a389923a632804dcaf39c575ea
Nathan Robel <nathanrobel6@gmail.com>
2026-05-20 16:48:40 +0100

    python list data

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     #“Go into the templates folder, find index.html, and send it to the browser.
-    return render_template("index.html")
+    tasks = ["Buy milk", "Study Flask"]
+    return render_template("index.html",tasks= tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
