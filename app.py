57df709825d8a477fe2f71df801202621741c50a
Nathan Robel <nathanrobel6@gmail.com>
2026-05-21 13:27:43 +0100

    update feature

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask,render_template,request,redirect
 
 app = Flask(__name__)
 tasks = []
-
+#Read
 @app.route("/")
 def home():
     #“Go into the templates folder, find index.html, and send it to the browser.
     
     return render_template("index.html",tasks= tasks)
 
+#Create
 @app.route("/add", methods = ["POST"])
 def add():
     new_task = request.form["task"]
@@ -17,6 +18,7 @@ def add():
     
     return redirect("/")
 
+#Delete
 @app.route("/delete", methods = ["POST"])
 def delete():
     task_id = int(request.form["task_id"])
@@ -26,6 +28,20 @@ def delete():
             break
     
     return redirect("/")
+
+#Update
+@app.route("/update", methods = ["POST"])
+def update():
+    task_id = int(request.form["task_id"])
+    updated_text = request.form["updated_task"]
+     
+    for task in tasks:
+        if task["id"] == task_id:
+            task["task"] = updated_text
+            break
+    
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
